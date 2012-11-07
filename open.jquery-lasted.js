e0d450be1ad4c2156e27a1d515977b7c6acabee9
hgouchet <herve.gouchet@twenga.com>
2012-11-07 14:08:50 +0100

    Rollback and merge with Open version with Ajax mode

--- open.jquery-lasted.js
+++ open.jquery-lasted.js
@@ -1,30 +1,45 @@
 /**
  * jQuery Open plugin
- *
- * @desc Enable to open links in iframe with loading message, in current view or
- *       in a new window
+ * 
+ * @desc Enable to open links in iframe with loading message, in ajax, in current view or in a new window
  * @author Hervé GOUCHET
  * @author Julien LEFEVRE
  * @requires jQuery 1.4.3+
  * @licenses Creative Commons BY-SA 2.0
  * @see https://github.com/rvflash/jQuery-Open
  */
-;
-(function($) {
+; 
+(function($)
+{
     var _workspace = {
         type : {
             self : 'self',
             blank : 'blank',
-            iframe : 'iframe'
+            iframe : 'iframe',
+            ajax : 'ajax'
         },
         onview : null,
-        addUniqueIdentifierClass : 'unique'
+        uniqueIdentifier : 'unique'
     };
-
+     
     var _defaults = {
-        type : _workspace.type.self, // self or blank or iframe, can be
+        type : _workspace.type.self,
         container : 'body',
-        // overload by className
+        ajax : {
+            name : '_openviewer',
+            loading : {
+                name : '_opencharger',
+                content : 'Please be patient, loading ...',
+                enable : true
+            },
+            error : {
+                name : '_openfailure',
+                content : 'An error was occured. Please to retry later.',
+                enable : true
+            },
+            displayInline : false,
+            toggle : true
+        },
         iframe : {
             name : '_openbrowser',
             options : {
@@ -56,9 +71,9 @@
         onExit : null,
         autoload : false
     };
-
-    var open = function(elem, settings) {
-        // Generate unique identifier
+    
+    var open = function(elem, settings)
+    {
         if (null == $(elem).data('_openid')) {
             $(elem).data('_openid', '_open' + Math.floor((Math.random() * 1001) + 1));
         }
@@ -71,11 +86,15 @@
             type = _workspace.type.blank;
         } else if ($(elem).hasClass(_workspace.type.iframe)) {
             type = _workspace.type.iframe;
+        } else if ($(elem).hasClass(_workspace.type.ajax)) {
+            type = _workspace.type.ajax;
         } else {
             type = settings.type;
         }
         if (null != (url = _url(elem, settings))) {
             switch (type) {
+                case _workspace.type.ajax:
+                    return _ajax(url, settings);
                 case _workspace.type.iframe:
                     return _iframe(url, settings);
                 case _workspace.type.blank:
@@ -87,22 +106,39 @@
         }
     };
 
-    var _iframe = function(url, settings) {
-        _workspace.onview = settings.name;
+    var _ajax = function (url, settings)
+    {
+        _buildBrowser(settings.name, settings.ajax);
 
-        // Build environment
-        if (0 == $('#' + settings.iframe.name).length) {
-            $(document.body).append('<div id="' + settings.iframe.name + '"></div>');
+        if (0 == $('#' + settings.name).length) {
+            $('#' + settings.ajax.name).append('<div id="' + settings.name + '"></div>');
+        } else if (false == settings.ajax.toggle) {
+            $('#' + settings.ajax.name + ' > div').hide();
         }
-        // Display loading message
-        if (settings.iframe.loading.enable) {
-            if (0 == $('#' + settings.iframe.loading.name).length) {
-                $(document.body).append('<div id="' + settings.iframe.loading.name + '">' + settings.iframe.loading.content + '</div>');
-            } else {
-                $('#' + settings.iframe.loading.name).show();
-            }
+
+        if ('' == $('#' + settings.name).html()) {
+            $('#' + settings.name).hide().html(' ');
+
+            return $.get(url, function(data)
+            {
+                $('#' + settings.name).html(data);
+            }).success(function()
+            {
+                _loadedView(settings.name, settings.ajax, settings.onExit);
+            }).error(function()
+            {
+                if (settings.ajax.error.enable) {
+                    $('#' + settings.ajax.error.name).show().delay(1000).hide();
+                }
+            });
         }
-        // Build iframe for this link
+        return _loadedView(settings.name, settings.ajax, settings.onExit);
+    };
+
+    var _iframe = function(url, settings)
+    {
+        _buildBrowser(settings.name, settings.iframe);
+
         if (0 == $('#' + settings.name).length) {
             var properties = [];
             var options = $.extend({}, settings.iframe.options, {
@@ -111,43 +147,29 @@
             for (opts in options) {
                 properties.push(opts + ' = "' + options[opts] + '"');
             }
-            var iframes = $('#' + settings.iframe.name + ' iframe');
+            // Keep a maximum number of iframe in DOM
+            var iframes = $('#' + settings.iframe.name + ' > iframe');
             if (settings.iframe.domMaxSize <= iframes.length) {
                 iframes.filter(':first').remove();
             }
             $('#' + settings.iframe.name).append('<iframe ' + properties.join(' ') + '></iframe>');
         }
-        $('#' + settings.iframe.name + ' iframe').hide();
+        $('#' + settings.iframe.name + ' > iframe').hide();
 
-        // Redirect to
         if (settings.iframe.loading.enable) {
             if ('' == $('#' + settings.name).attr('src')) {
                 return $('#' + settings.name).attr('src', url).load(function() {
-                    _iframeOpened(settings);
+                    _loadedView(settings.name, settings.iframe, settings.onExit);
                 });
             }
+        } else if ('' == $('#' + settings.name).attr('src')) {
+            $('#' + settings.name).attr('src', url);
         }
-        return _iframeOpened(settings);
+        return _loadedView(settings.name, settings.iframe, settings.onExit);
     };
 
-    var _iframeOpened = function(settings) {
-        if (_workspace.onview != settings.name) {
-            return;
-        }
-        if (settings.iframe.loading.enable) {
-            $('#' + settings.iframe.loading.name).hide();
-        }
-        if (settings.iframe.displayInline) {
-            $('#' + settings.name).show();
-        } else {
-            $('#' + settings.name).css('display', 'block');
-        }
-        if ($.isFunction(settings.onExit)) {
-            settings.onExit(settings.name);
-        }
-    };
-
-    var _popup = function(url, settings) {
+    var _popup = function(url, settings)
+    {
         var options = settings.popup;
         options.width = (screen.width * 0.80);
         if (options.width < 780) {
@@ -167,14 +189,65 @@
         return window.open(url, options.name, header.join(','));
     };
 
-    var _redirect = function(url, settings) {
+    var _redirect = function(url, settings)
+    {
         if ($.isFunction(settings.onExit)) {
             settings.onExit(settings.name);
         }
         return window.location.href = url;
     };
 
-    var _url = function(elem, settings) {
+    var _buildBrowser = function(name, options)
+    {
+        _workspace.onview = name;
+
+        // Build environment
+        if (0 == $('#' + options.name).length) {
+            $(document.body).append('<div id="' + options.name + '"></div>');
+        }
+        // Display loading message
+        if (options.loading.enable) {
+            if (0 == $('#' + options.loading.name).length) {
+                $(document.body).append('<div id="' + options.loading.name + '">' + options.loading.content + '</div>');
+            } else {
+                $('#' + options.loading.name).show();
+            }
+        }
+        // Display error message
+        if ('undefined' != typeof options.error && options.error.enable) {
+            if (0 == $('#' + options.error.name).length) {
+                $(document.body).append('<div id="' + options.error.name + '">' + options.error.content + '</div>');
+            }
+            $('#' + options.error.name).hide();
+        }
+    };
+
+    var _loadedView = function(name, options, callback)
+    {
+        if (_workspace.onview != name) {
+            return;
+        }
+        // Hide loading message
+        if (options.loading.enable) {
+            $('#' + options.loading.name).hide();
+        }
+        // Open viewer
+        if ('undefined' != typeof options.toggle && options.toggle && $('#' + name).is(':visible')) {
+            $('#' + name).hide();
+        } else if (options.displayInline) {
+            $('#' + name).show();
+        } else {
+            $('#' + name).css('display', 'block');
+        }
+        // Apply callback function 
+        if ($.isFunction(callback)) {
+            callback(name);
+        }
+    };
+
+    var _url = function(elem, settings)
+    {
+        // Extract url
         if (undefined != $(elem).attr('href')) {
             url = $(elem).attr('href');
         } else if (null != $(elem).data('url')) {
@@ -188,24 +261,25 @@
             } else {
                 url = sNrl;
             }
-        }
-        else
-        {
+        } else {
             return null;
         }
-        if ($(elem).hasClass(_workspace.addUniqueIdentifierClass)) {
+        // Add optional parameters
+        if (null != $(elem).data('arg')) {
+            url += (-1 != url.indexOf('?') ? '&' : '?') + $(elem).data('arg');
+        }
+        // Extend with an unique identifier
+        if ($(elem).hasClass(_workspace.uniqueIdentifier)) {
             url += (-1 != url.indexOf('?') ? '&' : '?') + '_i=' + new Date().getTime();
         }
         return url;
     };
 
-    var _rot13 = function(encoded) {
+    var _rot13 = function(encoded)
+    {
         var decoded = new String();
         var a, A, z, Z = new String();
-        a = "a";
-        A = "A";
-        z = "z";
-        Z = "Z";
+        a = "a"; A = "A"; z = "z"; Z = "Z";
 
         var i = -1, cc;
         while (i++ < encoded.length - 1) {
@@ -220,27 +294,30 @@
         return decoded;
     };
 
-    $.open = function(sSelector, settings) {
+    var _open = function (elem, e, settings)
+    {
+        e.preventDefault();
+        open(elem, settings);
+    };
+
+    $.open = function(elem, settings)
+    {
         var settings = $.extend(_defaults, settings);
-        // Using delegate on body
-        $(settings.container).delegate(sSelector, 'click', function(e){
-            e.preventDefault();
-            open(this, settings);
+        $(settings.container).delegate(elem, 'click', function(e)
+        {
+            _open(this, e, settings);
         });
     };
 
-    $.fn.open = function(settings) {
-
-        var settings = $.extend(_defaults, settings);
-
+    $.fn.open = function(settings)
+    {
+        var settings = $.extend({}, defaults, settings);
+        $(this).bind('click', function(e)
+        {
+            _open(this, e, settings);
+        });
         if (settings.autoload) {
             open($(this).filter(':first'), settings);
         }
-        $(this).bind('click', function(e) {
-            e.preventDefault();
-            open(this, settings);
-        });
-
     };
-
 })(jQuery);
